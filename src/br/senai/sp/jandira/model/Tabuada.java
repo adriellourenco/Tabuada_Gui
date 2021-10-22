f8bead0fdeede2f63dbfc274f191bfec098c66ee
ADRIEL LOURENÇO <21276092@CFP127ALUNO.LOCAL>
2021-10-22 16:55:06 -0300

    Aula do dia 22/10 - FPOO

---
+++ src/br/senai/sp/jandira/model/Tabuada.java
@@ -0,0 +1,25 @@
+package br.senai.sp.jandira.model;
+
+import java.util.Iterator;
+
+public class Tabuada {
+	
+	int multiplicando;
+	int multiplicador;
+	int resultado;
+	
+	public void setMultilplicando(int multiplicando){
+		this.multiplicando = multiplicando;
+	}
+	
+	public void setMultiplicador(int multiplicador) {
+		this.multiplicador = multiplicador;
+	}
+	
+	public void calcular() {
+		String contas[] = new String[multiplicador + 1]; 
+		for (int i = 0; i < multiplicador; i++) {
+			
+		}
+	}
+}
